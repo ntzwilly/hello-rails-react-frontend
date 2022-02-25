d05ed93381f1de1649978774318a0e7b70d63b09
ntzwilly <ntzwilly@gmail.com>
2022-02-25 11:14:39 +0200

    Create components and redux files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-function App() {
-  return (
-    <div className="App Container">
-      <h1>Welcome to the react App</h1>
-    </div>
-  );
-}
+import React from 'react';
+
+import {
+  BrowserRouter as Router, Routes, Route,
+} from 'react-router-dom';
+
+import LoadingBar from 'react-redux-loading-bar';
+
+import { Provider } from 'react-redux';
+
+import Greeting from './components/Greeting';
+
+import store from './redux/configureStore';
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <LoadingBar />
+      <Routes>
+        <Route exact path="/" element={<Greeting />} />
+      </Routes>
+    </Router>
+  </Provider>
+);
 
 export default App;
